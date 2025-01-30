7b55aa228f81c7db690659f98faa60e5fb6f134c
marcossilvestrini <marcos.silvestrini@gmail.com>
2025-01-29 22:10:54 -0300

    Update files for class today

---
+++ slack/example_silvestrini.py
@@ -0,0 +1,1 @@
+TOKEN="xoxp-foo=bar-beer"
---
+++ vagrant/destroy.ps1
@@ -0,0 +1,32 @@
+# Variables
+$WORKDIR =  $PSScriptRoot
+$vmPAth = "E:\Servers\VMware"
+$vms=(
+    "instance-1",
+    "instance-2"
+)
+
+# Set workdir
+Set-Location -Path $WORKDIR
+
+# Destroy instances
+vagrant destroy -f
+
+# Remove vagrant files
+If(Test-Path Vagrantfile) {
+    Write-Host "Vagrantfile removed"
+    Remove-Item -Path Vagrantfile -Recurse -Force
+}
+If(Test-Path ".vagrant") {
+    Write-Host "Folder .vagrant removed"
+    Remove-Item -Path ".vagrant" -Recurse -Force
+}
+
+# Remove VMs folders
+$vms | ForEach-Object {
+    $vmFolder = "$vmPAth\lpic3-topic-351-$_"    
+    If(Test-Path $vmFolder) {    
+        Write-Host "Folder $vmFolder removed"
+        Remove-Item -Path $vmFolder -Recurse -Force
+    }
+}
